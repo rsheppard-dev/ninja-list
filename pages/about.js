331101c56cedd6136639658e9ebc78e249a80595
rsheppard83 <rsheppard83@gmail.com>
2021-04-18 17:44:10 +0100

    initial

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+            </Head>
+
+            <div>
+                <h1>About</h1>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto deleniti est corrupti culpa alias consequuntur, cumque ipsum cupiditate enim nihil exercitationem laboriosam ipsam aspernatur dolorum sed adipisci, quam nostrum. Accusantium.</p>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto deleniti est corrupti culpa alias consequuntur, cumque ipsum cupiditate enim nihil exercitationem laboriosam ipsam aspernatur dolorum sed adipisci, quam nostrum. Accusantium.</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
